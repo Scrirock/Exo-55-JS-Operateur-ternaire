e2c407e749dcb8612f6adf8daec07be254a4cd11
Scrirock <73163680+Scrirock@users.noreply.github.com>
2020-11-25 11:53:43 +0100

    PLUS D'INNERHTML

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let span1 = parseInt(document.getElementById("sp1").innerHTML);
 let span2 = parseInt(document.getElementById("sp2").innerHTML);
 let result = document.getElementById("result");
 
-result.innerText = ((span1 + span2) === span1+span2) ? span1+span2 : "5+12 est different de 17";
-lorem.innerText = ((result - 10) < 0) ? "Attention (5 + 12) - 10 est strictement inferieur à 0" : lorem;
+result.innerHTML = ((span1 + span2) === span1+span2) ? span1+span2 : "5+12 est different de 17";
+lorem.innerHTML = ((result - 10) < 0) ? "Attention (5 + 12) - 10 est strictement inferieur à 0" : lorem;
 
 document.getElementById("sp1").style.color = "red";
 document.getElementById("sp2").style.color = "blue";
