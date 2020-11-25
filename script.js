e9dffc2bd0f28d351864d7d886abd3bce98ce345
Scrirock <73163680+Scrirock@users.noreply.github.com>
2020-11-25 11:15:52 +0100

    ghfdg

---
+++ script.js
@@ -0,0 +1,13 @@
+let lorem = document.getElementById("p1").textContent;
+
+let span1 = parseInt(document.getElementById("sp1").textContent);
+let span2 = parseInt(document.getElementById("sp2").textContent);
+let result = document.getElementById("result");
+
+result.innerText = ((span1 + span2) === span1+span2) ? span1+span2 : "5+12 est different de 17";
+lorem.innerText = ((result - 10) < 0) ? "Attention (5 + 12) - 10 est strictement inferieur à 0" : lorem;
+
+document.getElementById("sp1").style.color = "red";
+document.getElementById("sp2").style.color = "blue";
+document.getElementById("p1").style.backgroundColor = "antiqueWhite";
+
